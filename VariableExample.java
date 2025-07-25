14308d5f2c180246994125baba95e1014c531a95
thapthep <684259038@webmail.npru.ac.th>
2025-07-25 12:27:23 +0700

    Add files via upload

---
+++ VariableExample.java
@@ -0,0 +1,25 @@
+public class VariableExample {
+    public static void main(String[] args) {
+        char clothingSize = 's'; // M for Medium
+
+        int websiteVisitors = 0; // Number of visitors to the website
+
+        char studentGrade = 'A'; // Grade of the student
+
+        double studentGPA = 4.0; // GPA of the student
+
+        final double PI = 3.14159; // Value of Pi, constant
+
+        float annualExpenses = 15000.00f; // Annual expenses in dollars
+        
+        final int MONTHS_IN_YEAR = 12; // Constant for number of months in a year
+        
+        System.out.println("Clothing Size: " + clothingSize);
+        System.out.println("Website Visitors: " + websiteVisitors);
+        System.out.println("Student Grade: " + studentGrade);
+        System.out.println("Student GPA: " + studentGPA);
+        System.out.println("Value of Pi: " + PI);
+        System.out.println("Annual Expenses: " + annualExpenses);
+        System.out.println("Months in a Year: " + MONTHS_IN_YEAR);
+    }
+}
