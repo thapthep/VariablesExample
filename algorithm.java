7d7b69bcafe652f7392d610450eb5559b37213eb
thapthep <684259038@webmail.npru.ac.th>
2025-07-26 16:57:03 +0700

    Add files via upload

---
+++ algorithm.java
@@ -0,0 +1,8 @@
+public class algorithm {
+    public static void main(String[] args) {
+        int radius = 5; // Radius of the circle
+        int area = (int) (Math.PI * radius * radius); // Area of the circle
+        System.out.println("Area of circle that has radius = " + radius + " is: " + area);
+    }
+    
+}
